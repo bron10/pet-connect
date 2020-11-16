682347acec4816de5161942a6a4014519e77efea
Bron10 <bron1010@gmail.com>
2020-11-16 16:46:25 +0530

    1st prototype

--- src/modules/customers/customers.controller.ts
+++ src/modules/customers/customers.controller.ts
@@ -3,23 +3,21 @@ import { CustomersService } from './customers.service';
 import {
   ApiBearerAuth,
   ApiOperation,
-  ApiResponse,
-  ApiTags,
-  ApiQuery
 } from '@nestjs/swagger';
 import { CustomerQueryDto } from './customers.dto';
 
 @Controller('customers')
 export class CustomersController {
   constructor(private customersService: CustomersService) {}
-  // @ApiQuery({
-  //   email : 
-  // })
+  
+  /**
+   * 
+   * @param query query parameters contains {state, name, email, petExperience}
+   */
   @ApiOperation({ summary: 'Fetch customers by `state, pet_experience, names, email`' })
   @Get()
   async customer(@Query() query : CustomerQueryDto) {
     const {state, name, email, petExperience} = query;
     return this.customersService.findAll({state, name, email, petExperience});
-    // return await this.customersService.findAll();
   }
 }
